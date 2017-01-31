ea1ce8eb44efcac36c558ae48eab1fe2c80cf4ad
Daniel Braimah <osebraimah@Daniels-MacBook-Pro-2.local>
2017-01-30 22:42:14 -0500

    First sample fortran source code

---
+++ sample.f
@@ -0,0 +1,10 @@
+!    First Program in fortran
+
+      program sample
+      real r, area
+      write (*,*) 'radius r: '
+      read (*,*) r
+      area = 3.14159*r*r
+      write (*,*) 'Area = ', area
+
+      end program sample
